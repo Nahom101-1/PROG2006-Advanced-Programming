7fa2886c5a3dc58727928f6d34796d0f3b6b7476
Nahom <nahomberhane101@gmail.com>
2025-05-19 12:59:54 +0200

    remove commented code and move do-block

--- Haskell/labs/lab05/test/Spec.hs
+++ Haskell/labs/lab05/test/Spec.hs
@@ -1,14 +1,18 @@
-module Main (main) where
+module Main where
 
 import Test.Hspec
-import Task1Spec      -- keep only this
--- import Task2Spec  -- comment out
--- import Task3Spec  -- comment out
--- import Task4Spec  -- comment out
+
+import qualified Task1Spec
+import qualified Task2Spec
+import qualified Task3Spec
+import qualified Task4Spec
 
 main :: IO ()
-main = hspec $ do
+main = hspec spec
+
+spec :: Spec
+spec = do
   Task1Spec.spec
---  Task2Spec.spec
---  Task3Spec.spec
---  Task4Spec.spec
+  Task2Spec.spec
+  Task3Spec.spec
+  Task4Spec.spec
