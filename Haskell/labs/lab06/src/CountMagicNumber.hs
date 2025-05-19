67271d75446e93c9fcea258568f4d779d95087a9
Nahom <nahomberhane101@gmail.com>
2025-05-19 14:58:45 +0200

    Lab06: implement Galactic Greetings decoder with  doctests

--- Haskell/labs/lab06/src/CountMagicNumber.hs
+++ Haskell/labs/lab06/src/CountMagicNumber.hs
@@ -1,10 +1,9 @@
 module CountMagicNumber where
-
-
 import Count ( count )
 
-countMagicNumber :: [Int] -> Int -> Either String Int
-countMagicNumber xs magic = Right (count magic xs)
+-- | Count how many times a magic number appears in a list.
+countMagicNumber :: [Int] -> Int -> Either String Int --
+countMagicNumber xs magic = Right (count magic xs) -- uses the count function to count occurrences of magic number in the list
 -- | Count how many times magic number appears
 -- >>> countMagicNumber [1,2,3,4,5] 3
 -- Right 1
--- Haskell/labs/lab06/src/Decoder.hs
+++ Haskell/labs/lab06/src/Decoder.hs
@@ -1,14 +1,15 @@
 module Decoder (decodeMessage) where
     
 import UniqueMinMax ( uniqueMinMax )
-
 import CheckEvenSum ( checkEvenSum )
-
 import CountMagicNumber ( countMagicNumber )
 
+-- | Decode the alien message from a list of integers.
+-- The message is the count of the magic number, which is the average of the minimum and maximum values in the list.
 decodeMessage :: [Int] -> Either String Int
-decodeMessage xs = uniqueMinMax xs >>= checkEvenSum >>= countMagicNumber xs
-
+decodeMessage xs = uniqueMinMax xs >>= checkEvenSum >>= countMagicNumber xs -- Here we use the uniqueMinMax function to get the minimum and maximum values from the list, 
+--then check if their sum is even. If it is, we count how many times the average (magic number) appears in the list.
+    
 -- | Decode the alien message from the list of integers
 -- >>> decodeMessage [5,5,5,8,1,2,3,4,9,8,2,3,4]
 -- Right 3
